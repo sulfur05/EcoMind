fd2aa6cfdf5479f15d05dc0d66a4f547bb16d7c9
sulfur05 <riyacodes05@gmail.com>
2024-04-05 23:35:16 +0530

    routing donE

---
+++ Web/src/components/Articles/Articles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Articles</div>
+  )
+}
+
+export default Navbar;
---
+++ Web/src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Navbar } from './Navbar/Navbar'
+
+export{default as Home} from './Home/Home'
+
+export{default as Articles} from './Articles/Articles'
+export {default as Products} from './Products/Products'
+
+export {default as ChatRoom} from './ChatRoom/ChatRoom'
